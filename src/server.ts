203c939a60ee8caafac99ad90b000832edbf8b35
Isaque Hernandes <isantana@delirec.com>
2023-03-03 22:15:30 -0300

    remove not used code

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,6 @@
 import express, { Express } from 'express';
-// import mongoose from "mongoose"
 import cors from 'cors'
 import routes from './routes/index';
-
-// import { redisConnection } from "@services/redis";
-// import { log } from '@utils/log';
 
 const server = async () => {
   const app: Express = express()
@@ -14,17 +10,6 @@ const server = async () => {
   const port = process.env.PORT ?? 3333
 
   routes(app)
-
-  // mongoose.connect(process.env.MONGO_URL).then(() => {
-  //   log.info('MongoDB connected')
-  // }).catch(err => {
-  //   log.error('MongoDB error: ', err)
-  // })
-
-  // redisConnection.connect()
-  //   .then(() => {
-  //     log.info("Redis Up")
-  //   }).catch(log.error)
 
   app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
