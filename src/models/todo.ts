f44d3aaea394f7fa574384d5c6ab419fde8be932
Isaque Hernandes <isantana@delirec.com>
2023-03-05 21:32:17 -0300

    change entry model to be todo model

---
+++ src/models/todo.ts
@@ -0,0 +1,19 @@
+import { Todo } from "@prisma/client"
+import { Validation } from "@validations/validator"
+
+interface TodoChangeValidationFields extends Omit<Todo, "id" | "userId" | "createdAt" | "updatedAt"> {}
+
+export type TodoChangeValidation = Validation<TodoChangeValidationFields>
+
+interface ITodoModel {
+  changeValidation: TodoChangeValidation
+}
+
+export const entryValidations: ITodoModel = {
+  changeValidation: {
+    title: { name: "Title", type: "string", required: true, min: 3, max: 255 },
+    date: { name: "Date", type: "date", required: true },
+    isDone: { name: "Done", type: "boolean", required: true },
+    priority: { name: "Priority", type: "number", min: 1, max: 3 },
+  }
+}
